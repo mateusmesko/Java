fbae5db7f6e8b818dcc2c231a049862f707315bc
Mateus Mesko <58198358+mateusmesko@users.noreply.github.com>
2021-09-19 22:26:48 -0300

    Add files via upload

---
+++ Comentario.java
@@ -0,0 +1,30 @@
+package fundamentos;
+
+/**
+ * Classe responsavel por mostrar o uso dos 3 tipos de comentarios
+ * que o Java suporta.
+ * 
+ * @author Leonardo Leitão [leonardomleitao@gmail.com]
+ * @version 1.0
+ * @since 2.2
+ */
+public class Comentario {
+	
+	/**
+	 * Esse metodo é a porta de entrada de um programa
+	 * 
+	 * @param args parametros passados por linha de comando
+	 */
+	
+	public static void main(String[] args) {
+		/* 
+		 * Imprime no console a palavra 'primeiro' e
+		 *  em seguida cria uma nova linha
+		 */
+		System.out.println("Primeiro");
+		
+		// Cuidado com comentarios obvios
+		System.out.println("Programa");
+	}
+ 
+}
