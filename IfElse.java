acc43fef14c20674b5c3a61c4e845951e16f957e
Mateus Mesko <58198358+mateusmesko@users.noreply.github.com>
2021-09-23 21:52:13 -0300

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,15 @@
+package controle;
+
+public class IfElse {
+
+    public static void main(String[] args){
+        double nota = 6.5;
+
+        if(nota >= 7){
+            System.out.println("Aprovado!");
+            System.out.println("Parabens!");
+        } else {
+            System.out.println("Recuperação");
+        }
+    }
+}
