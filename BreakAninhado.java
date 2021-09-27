f8819ab07d49be9a5f6260812e7f9d342bd1fe2d
Mateus Mesko <58198358+mateusmesko@users.noreply.github.com>
2021-09-26 22:21:20 -0300

    Update BreakAninhado.java

--- BreakAninhado.java
+++ BreakAninhado.java
@@ -5,7 +5,7 @@ public class BreakAninhado {
     public static void main(String[] args) {
         externo: for (int i = 0; i < 5; i++) {
             for(int j = 0; j < 5; j++) {
-                if(j == 2) {
+                if(j == 2){
                     break externo;
                 }
 
