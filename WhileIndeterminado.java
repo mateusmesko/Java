2ca6a6b59cfc776e9873d2e0813d8e8f8772d41f
Mateus Mesko <58198358+mateusmesko@users.noreply.github.com>
2021-10-17 23:55:33 -0300

    Update WhileIndeterminado.java

--- WhileIndeterminado.java
+++ WhileIndeterminado.java
@@ -2,7 +2,7 @@ package controle;
  
 import java.util.Scanner;
 
-public class WhileIndeterminado {
+public class WhileIndeterminado { 
  
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
